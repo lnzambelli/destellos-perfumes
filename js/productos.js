4a91e3b58ba1dc7753e4e9819caa605fcc0decc7
Luis Zambelli <lnzambelli@gmail.com>
2022-04-06 22:58:34 -0300

    se agrego carrito renderizado

--- js/productos.js
+++ js/productos.js
@@ -1,52 +1,61 @@
+
 class Perfume{
-    constructor(codId, nombre, fragancia, precio, categoria){
-        this.codId = codId;
+    constructor(nombre, fragancia, precio, categoria){
         this.nombre = nombre.toUpperCase();
         this.fragancia = fragancia.toLowerCase();
         this.categoria = categoria.toUpperCase();
         this.precio = Number(precio);
         this.urlImg = "./../img/foto-perfume.webp";
-        this.cantidad= 0;
+    }
+}
+
+class Carrito{
+    constructor(nombre, fragancia, precio){
+        this.nombre = nombre.toUpperCase();
+        this.fragancia = fragancia.toLowerCase();
+        this.precio = precio.replace('$','');
+        this.cantidad = 1;
+        this.total = Number(this.precio)*this.cantidad;
     }
 }
 
 const arrayPerfumes = [];
 
 const cargarArray= () =>{
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Carolina Herrera", "212 men",16900,"Hombre"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Natura", "mad men",10900,"Hombre"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Natura", "aaaa",9500,"Hombre"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Natura", "bbbb",15000,"Hombre"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Loewe", "Agua Ella",13200,"Mujer"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Loewe", "Aire",15675,"Mujer"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Versace", "Bright Crystal",16320,"Mujer"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Kaloo", "Kaloo blue",2450,"Niños"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Kaloo ", "Kaloo red",2950,"Niños"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Kaloo", "Kaloo white",2000,"Niños"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Kaloo ", "Kaloo black",2900,"Niños"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Calvin Klein", "Ck one",16900,"Unisex"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Calvin Klein", "Ck one new",14900,"Unisex"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Calvin Klein", "Ck one old",12500,"Unisex"));
-    arrayPerfumes.push(new Perfume(arrayPerfumes.length,"Calvin Klein", "Ck one bb",13600,"Unisex"));
+    arrayPerfumes.push(new Perfume("Carolina Herrera", "212 men",16900,"Hombre"));
+    arrayPerfumes.push(new Perfume("Natura", "mad men",10900,"Hombre"));
+    arrayPerfumes.push(new Perfume("Ntaaa", "aaaa",9500,"Hombre"));
+    arrayPerfumes.push(new Perfume("Aaaaa", "bbbb",15000,"Hombre"));
+    arrayPerfumes.push(new Perfume("Loewe", "Agua Ella",13200,"Mujer"));
+    arrayPerfumes.push(new Perfume("Loewe aaa", "Aire",15675,"Mujer"));
+    arrayPerfumes.push(new Perfume("Versace", "Bright Crystal",16320,"Mujer"));
+    arrayPerfumes.push(new Perfume("Kaloo aa", "Kaloo blue",2450,"Niños"));
+    arrayPerfumes.push(new Perfume("Kaloo bb", "Kaloo red",2950,"Niños"));
+    arrayPerfumes.push(new Perfume("Kaloo cc", "Kaloo white",2000,"Niños"));
+    arrayPerfumes.push(new Perfume("Kaloo dd", "Kaloo black",2900,"Niños"));
+    arrayPerfumes.push(new Perfume("Calvin K", "Ck one",16900,"Unisex"));
+    arrayPerfumes.push(new Perfume("Calvin Klein", "Ck one new",14900,"Unisex"));
+    arrayPerfumes.push(new Perfume("Calvin Kl", "Ck one old",12500,"Unisex"));
+    arrayPerfumes.push(new Perfume("Calvin", "Ck one bb",13600,"Unisex"));
 }
 
 //RRECORREMOS EL ARRAY Y MOSTRAMOS EN EL DOCUMENT
 const mostrarArray =(arrayListado) => {
     let contenedorDeProductos = document.getElementById("listadoDeProductos");
-    
+
     for(let perfume of arrayListado){
         let nuevoPerfume = document.createElement('div');
         nuevoPerfume.innerHTML = ` <div class="col" >
-        <div class="card h-100" id="eliminado">
-        <img src=${perfume.urlImg} class="card-img-top w-50 m-auto" alt="...">
-        <div class="card-body">
-        <h5 class="card-title text-center">${perfume.nombre}</h5>
-        <p class="card-text text-center">${perfume.fragancia}</p>
-        <div class="d-flex justify-content-center">
-            <button type="button" class="btn rounded-pill">$${perfume.precio} <i class="bi bi-cart-plus"></i></button>
-        </div>
-        </div>
-        </div> 
+            <form class="card h-100 formCard">
+            <img src=${perfume.urlImg} class="card-img-top w-50 m-auto" alt="...">
+            <div class="card-body">
+            <h5 class="card-title text-center">${perfume.nombre}</h5>
+            <p class="card-text text-center">${perfume.fragancia}</p>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn rounded-pill btnAgregar">$${perfume.precio} <i class="bi bi-cart-plus"></i></button>
+            </div>
+            </div>
+            </form> 
         </div>`;
         contenedorDeProductos.append(nuevoPerfume)
     }
@@ -54,7 +63,7 @@ const mostrarArray =(arrayListado) => {
 
 
 const filtrarPorCategoria = (categSeleccionada, idListado) =>{
-    let arrayFiltro = [];
+   
     let contenedorDeProductos = document.getElementById(idListado);
     contenedorDeProductos.innerHTML = '';
     
@@ -62,16 +71,16 @@ const filtrarPorCategoria = (categSeleccionada, idListado) =>{
     for(let perfume of arrayFiltro){
         let nuevoPerfume = document.createElement('div', );
         nuevoPerfume.innerHTML = ` <div class="col" >
-        <div class="card h-100" id="eliminado">
-        <img src=${perfume.urlImg} class="card-img-top w-50 m-auto" alt="...">
-        <div class="card-body">
-        <h5 class="card-title text-center">${perfume.nombre}</h5>
-        <p class="card-text text-center">${perfume.fragancia}</p>
-        <div class="d-flex justify-content-center">
-            <button type="button" class="btn rounded-pill">$${perfume.precio} <i class="bi bi-cart-plus"></i></button>
-        </div>
-        </div>
-        </div> 
+            <form class="card h-100 formCard">
+            <img src=${perfume.urlImg} class="card-img-top w-50 m-auto" alt="...">
+            <div class="card-body">
+            <h5 class="card-title text-center">${perfume.nombre}</h5>
+            <p class="card-text text-center">${perfume.fragancia}</p>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn rounded-pill btnAgregar">$${perfume.precio} <i class="bi bi-cart-plus"></i></button>
+            </div>
+            </div>
+            </form> 
         </div>`;
         contenedorDeProductos.append(nuevoPerfume);
     }
@@ -101,15 +110,50 @@ const buscarPorNombre = (nombreDePerfume) =>{
 CARRITO DE COMPRA
 ----------------------------------------------------*/
 
-const arrayCarrito = [];
+let arrayCarrito = [];
 
 const agregarAlCarrito= (perfume) =>{
     //condicion: si existe el producto sumo cant++ sino lo agrego
-    this.arrayCarrito.push(perfume)
+    let nuevo = new Carrito(perfume[0], perfume[1], perfume[2])
+    if (arrayCarrito.length==0){
+        arrayCarrito.push(nuevo)
+    }else{
+        arrayEncontrado = arrayCarrito.filter(dato => dato.nombre==nuevo.nombre);
+        
+        if(arrayEncontrado.length==0){
+            arrayCarrito.push(nuevo)
+        }
+        else{
+            arrayCarrito.forEach(dato =>{
+                if(dato.nombre==nuevo.nombre){
+                    dato.cantidad++;
+                    dato.total = dato.cantidad * dato.precio
+                }
+            })
+        }
+        
+    }
+    mostrarCarrito(arrayCarrito)
+    console.log(arrayCarrito)
 }
 
-const obtenerPrecioTotal = () =>{
-    const total = arrayCarrito.reduce((accumulator, curValue) =>accumulator + curValue.precio, 0)
+const mostrarCarrito = (arrayCarrito) =>{
+    let contenedorCarrito = document.getElementById('listaCarrito');
+    contenedorCarrito.innerHTML = '';
+    for(let cart of arrayCarrito){
+        let nuevalista = document.createElement('li');
+        nuevalista.innerHTML = ` <li class="list-group-item">${cart.cantidad} ${cart.nombre} ${cart.fragancia} $${cart.total}</li>`;
+        contenedorCarrito.appendChild(nuevalista);
+    }
+    let total = obtenerPrecioTotal(arrayCarrito)
+    let nuevalista = document.createElement('li');
+    nuevalista.innerHTML = ` <li class="list-group-item list-group-item-danger">TOTAL: $${total}</li>`;
+    contenedorCarrito.appendChild(nuevalista);
+    
+}
+
+const obtenerPrecioTotal = (curValue) =>{
+    return arrayCarrito.reduce((accumulator, curValue) =>accumulator + curValue.total, 0)
 }
 
 const eliminarPerfumeDelCarrito = (perfAEliminar) =>{
@@ -155,3 +199,24 @@ botonCategoriaUnisex.onclick = ()=>{
     let idListado = 'listadoDeProductosUnisex';
     filtrarPorCategoria(botonCategoriaUnisex.innerText, idListado) ;
 }
+
+let dataPerfume = document.getElementsByClassName("formCard");
+
+for(let perfume of dataPerfume){
+    perfume.addEventListener('submit',enviarDatos); 
+}
+
+function enviarDatos(e){
+    e.preventDefault();
+    //obtener el dato
+    let dato = e.target;
+    
+   let prod = (dato.children[1].innerText);
+
+   let newCartPerfume = prod.split('\n\n');
+
+   agregarAlCarrito(newCartPerfume);
+}
+
+
+
